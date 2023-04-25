a6842bd13de6d64bb72dcc523bc50ade5626ccfc
Pedro Passos <pccp001@bucknell.edu>
2023-04-25 01:49:58 -0400

    fixed app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
-app = Flask(__main__)
+app = Flask(__name__)
 
-@app.route("/") #this is your "home" route
-def index():
-    return "Congrats, this is the beginning of a great server!"
+@app.route('/')
+def hello_world():
+    return 'Congrats on your first web server!'
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     app.run(host="127.0.0.1", port=1025) #change the port to the port assigned to your project
+
+
